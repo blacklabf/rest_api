77c29a8aaf84684ebf68e3ef6897aabf5bcd5656
soyoung <bjo8244@naver.com>
2024-01-16 23:07:48 +0900

    Ristrict input for POST /api/events

--- event/src/test/java/soyoung/eventrestapi/events/EventControllerTests.java
+++ event/src/test/java/soyoung/eventrestapi/events/EventControllerTests.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
@@ -21,17 +23,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
-@WebMvcTest // web과 관련된 bean들 모두 등록
+//@WebMvcTest // slicing Test
+@SpringBootTest// 실제 repository 값 사용해서 적용 됨.
+@AutoConfigureMockMvc
 public class EventControllerTests { // 단위테스트는 아님 (9강-2:40)
 
     @Autowired
-    MockMvc mocMvc; // spring MVC
+    MockMvc mocMvc; // SpringBootTest - MockMVC용 테스트가 설정
 
     @Autowired
     ObjectMapper objectMapper;
-
-    @MockBean
-    EventRepository eventRepository;
 
     @Test
     public void createEvent() throws Exception {
@@ -52,19 +53,13 @@ public class EventControllerTests { // 단위테스트는 아님 (9강-2:40)
                 .free(true)
                 .offline(false)
                 .build();
-//        event.setId(10); - Id를 10으로 mocking
-        // nullpointException 아래에서 mocking을 할 떄 event를 저장할 때 event를 리턴하게 했는데 - 실제 event를 받는게 아니라 실제로 save에 전달한 객체는 createEvent에서 새로 만든 object (여기 위에 event가 아님)
-//        Mockito.when(eventRepository.save(event)).thenReturn(event); // nullPointerException 방지
-
-        // mocking 안 함
-
-//        mocMvc.perform(post("/api/events").contentType(MediaType.APPLICATION_JSON).accept(MediaTypes.HAL_JSON).content(objectMapper.writeValueAsString(event))).andDo(print()).andExpect(status().isCreated()).andExpect(jsonPath("id").exists()).andExpect(header().exists("Location")) // Location header 있는지 확인
-//                .andExpect(header().string("Content-Type", "application/hal+json"));
 
         mocMvc.perform(post("/api/events").contentType(MediaType.APPLICATION_JSON).accept(MediaTypes.HAL_JSON).content(objectMapper.writeValueAsString(event))).andDo(print()).andExpect(status().isCreated()).andExpect(jsonPath("id").exists()).andExpect(header().exists(HttpHeaders.LOCATION)) // Location header 있는지 확인
                 .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
                 .andExpect(jsonPath("id").value(Matchers.not(100)))
-                .andExpect(jsonPath("free").value(Matchers.not(true)));
+                .andExpect(jsonPath("free").value(Matchers.not(true)))
+                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()))
+        ;
     }
 
 
